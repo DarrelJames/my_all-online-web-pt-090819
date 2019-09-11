85bc79f9d904953750709f41e01c69165a684c24
Darrel C <dcastellano2007@gmail.com>
2019-09-11 06:33:12 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,8 +2,15 @@ require 'pry'
 
 def my_all?(collection)
   i = 0
+  newarr = []
   while i < collection.length
-    yield(collection[i])
+    newarr << yield(collection[i])
     i += 1
+  end
+
+  if block_return_values.include?(false)
+    false
+  else
+    true
   end
 end
